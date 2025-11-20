99a88dcadf9454a5c9ed65b276c641df5bdc6079
FoXoDoX <135237101+FoXoDoX@users.noreply.github.com>
2025-11-20 23:16:47 +0500

    Add endless background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+
+    void Update()
+    {
+        transform.position = target.position + offset;        
+    }
+}
